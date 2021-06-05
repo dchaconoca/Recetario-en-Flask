a2f6dc0702f073f26dbaa83a119dd76d5bb15dd6
Diana Chacon <dchacon74@gmail.com>
2021-06-05 17:43:15 -0400

    gestion receta

---
+++ app/recetario/gestion_recetas.py
@@ -0,0 +1,11 @@
+#########################################################
+# GESTIÓN DE RECETAS
+# Módulo donde se definen todas las reglas de negocio
+# para la gestión de las recetas, sus ingredientes y
+# el cálculo de sus costos y precios
+# De esta manera, models.py se encarga de definir los
+# modelos y la comunicación con la base de datos
+# y routes.py se encarga únicamente de llamar el 
+# presente módulo y gestionar la comunicación con 
+# el front
+#########################################################
