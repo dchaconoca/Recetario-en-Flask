146f65553f822fa125283e02a03eaefbed453652
dchaconoca <dchacon74@gmail.com>
2021-09-30 18:34:23 -0400

    Prueba antes cambio directorio

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ import os
 
 settings_module = os.getenv('APP_SETTINGS_MODULE')
 
-# print("settings_module", settings_module)
+if not settings_module:
+    settings_module = 'config.DevelopmentConfig'
+
+print("settings_module", settings_module)
 
 app = create_app(settings_module)
 app.run()
