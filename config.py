aef7ff76bd832810f72d6884fce5c322b9593292
dchaconoca <dchacon74@gmail.com>
2021-09-29 21:19:05 -0400

    Seguimos con Heroku

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
-# from decouple import config
+#from decouple import config
+
+import os
 
 class Config:
     SECRET_KEY = 'fjaf343efwereb1g5eere5rhrtery'
@@ -9,6 +11,7 @@ class DevelopmentConfig(Config):
     DEBUG = True
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = config('DATABASE_URL', default='localhost')
-    #SQLALCHEMY_DATABASE_URI ='postgresql://gxedmqdfuiwtfg:e181db08b929e912e1f41708f6801f16601b253670cd9ca43a7f2a8ac4d93a5c@ec2-54-174-172-218.compute-1.amazonaws.com:5432/db74dnhp8cakq2'
+    #SQLALCHEMY_DATABASE_URI = app.config('DATABASE_URL', default='localhost')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    # SQLALCHEMY_DATABASE_URI ='postgresql://gxedmqdfuiwtfg:e181db08b929e912e1f41708f6801f16601b253670cd9ca43a7f2a8ac4d93a5c@ec2-54-174-172-218.compute-1.amazonaws.com:5432/db74dnhp8cakq2'
     DEBUG = True
