acf45fe6fcbef7bdc750589efceb4f1f22bcb3be
dchaconoca <dchacon74@gmail.com>
2021-10-01 10:49:33 -0400

    Modif DATABASE_URI

--- main.py
+++ main.py
@@ -10,14 +10,10 @@ settings_module = os.getenv('APP_SETTINGS_MODULE')
 if not settings_module:
     settings_module = 'config.DevelopmentConfig'
 
-# print("settings_module", settings_module)
-
 app = create_app(settings_module)
 
 if __name__ == '__main__':
     app.run()
 
-# port = int(os.environ.get('PORT', 5000))
-# app.run(host='0.0.0.0', port=port)
 
 
