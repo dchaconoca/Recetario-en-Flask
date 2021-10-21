c36430cfe71afb83012e09e0437472aa207a85c5
dchaconoca <dchacon74@gmail.com>
2021-10-20 22:54:23 -0400

    Funcionamiento de la aplicacion

--- app/__init__.py
+++ app/__init__.py
@@ -16,17 +16,10 @@ migrate = Migrate()
 def create_app(settings_module='config.DevelopmentConfig'):
     app = Flask(__name__)
 
-    # print("settings_module", settings_module)
-
     app.config.from_object(settings_module)
-
-    # print("config", app.config)
-    
 
     # Se agregó "pymysql" para que funcionara, pues por defecto utiliza mysqlclient
     # que me da un error al instalarlo, aparentemente no funciona en 32 bits
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://diana:Color74@localhost/ProyectoFlask?charset=utf8mb4'
-    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
     migrate.init_app(app, db)
--- config.py
+++ config.py
@@ -7,12 +7,9 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(Config):
-    # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://diana:Color74@localhost/ProyectoFlask?charset=utf8mb4'
     SQLALCHEMY_DATABASE_URI = 'postgresql://diana:Color74@localhost:5432/RecetarioFlask'
     DEBUG = True
 
 class ProductionConfig(Config):
-    #SQLALCHEMY_DATABASE_URI = app.config('DATABASE_URL', default='localhost')
-    # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_DATABASE_URI ='postgresql://gxedmqdfuiwtfg:e181db08b929e912e1f41708f6801f16601b253670cd9ca43a7f2a8ac4d93a5c@ec2-54-174-172-218.compute-1.amazonaws.com:5432/db74dnhp8cakq2'
     DEBUG = True
